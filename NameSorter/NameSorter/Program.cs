941ea8e9d59249eda4886a9b536920e4342766d4
Krisna Adji Syahputra <adji.kontak@gmail.com>
2020-01-07 00:21:04 +0700

    Sorting by last name

---
+++ NameSorter/NameSorter/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NameSorter
+{
+    public class ReadWriteFile
+    {
+        public List<string> ReadFileToList(string fileName)
+        {
+            // Read txt file and store to List<string>
+            List<string> read = File.ReadAllLines(fileName).ToList();
+            return read;
+        }
+
+        public void WriteListToFile(string resultName, List<string> strList)
+        {
+            // Write txt file from List<string> type data
+            using (StreamWriter writer = new StreamWriter(resultName))
+            {
+                strList.ForEach(writer.WriteLine);
+            }
+        }
+    }
+
+    public class PrintList
+    {
+        public void Print(List<string> strList)
+        {
+            strList.ForEach(Console.WriteLine);
+        }
+    }
+
+    public class ReverseWords
+    {
+        // 
+        public string ReverseWordInString(string[] s)
+        {
+            string lastToGiven = s[s.Length-1];
+            for (int i = 0; i < s.Length-1; i++)
+            {
+                lastToGiven += " " + s[i];
+            }
+            Console.WriteLine(lastToGiven);
+            return lastToGiven;
+        }
+    }
+
+    public class PairLists : ReverseWords
+    {
+        private static int _minLength = 2;
+        private static int _maxLength = 4;
+
+        public List<KeyValuePair<string, string>> MakePair(List<string> origName)
+        {
+            List<KeyValuePair<string, string>> pair = new List<KeyValuePair<string, string>>();
+            foreach (string value in origName)
+            {
+                string[] s = value.Split(' ');
+                if (s.Length < _minLength || s.Length > _maxLength) continue;
+
+                pair.Add(new KeyValuePair<string, string>(ReverseWordInString(s), value));
+            }
+            return pair;
+        }
+    }
+
+    public class Sorter
+    {
+        private string _resultFileName;
+        private List<string> _originalNameList, _sortedNameList;
+
+        ReadWriteFile readWriteFile = new ReadWriteFile();
+        PairLists pairs = new PairLists();
+        PrintList p = new PrintList();
+
+        public Sorter(string fileName)
+        {
+            _originalNameList = readWriteFile.ReadFileToList(fileName);
+            _resultFileName = "sorted-names-list.txt";
+        }
+
+        public void SetResultFileName(string newName)
+        {
+            _resultFileName = newName;
+        }
+
+        public void SortName()
+        {
+            List<KeyValuePair<string, string>> pairName = pairs.MakePair(_originalNameList);
+            _sortedNameList = pairName.OrderBy(x => x.Key).Select(y => y.Value).ToList();
+            p.Print(_sortedNameList);
+            readWriteFile.WriteListToFile(_resultFileName, _sortedNameList);
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Sorter nm = new Sorter(args[0]);
+            nm.SortName();
+        }
+    }
+}
---
+++ NameSorter/UnitTestNunit/Test.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using NameSorter;
+
+namespace UnitTestNunit
+{
+    [TestFixture()]
+    public class Test
+    {
+        List<string> testList = new List<string>();
+        [Test()]
+        public void ReadTest()
+        {
+            ReadWriteFile read = new ReadWriteFile();
+            testList = read.ReadFileToList("unsorted.txt");
+        }
+
+        [Test()]
+        public void WriteTest()
+        {
+            ReadWriteFile write = new ReadWriteFile();
+            write.WriteListToFile("write.txt", testList);
+        }
+
+        [Test()]
+        public void PrintTest()
+        {
+            PrintList p = new PrintList();
+            p.Print(testList);
+        }
+
+        [Test()]
+        public void Reverse()
+        {
+            ReverseWords rev = new ReverseWords();
+            string[] s = "This is unit testing".Split(' ');
+            string reversedStr = rev.ReverseWordInString(s);
+            Assert.AreEqual("testing This is unit", reversedStr);
+        }
+
+        [Test()]
+        public void SorterTest()
+        {
+            Sorter n = new Sorter("unittest.txt");
+            n.SetResultFileName("unittestsorted.txt");
+            n.SortName();
+
+            ReadWriteFile read = new ReadWriteFile();
+            List<string> correct = read.ReadFileToList("sortedtest.txt");
+            List<string> test = read.ReadFileToList("unittestsorted.txt");
+            Assert.AreEqual(correct, test);
+        }
+    }
+}
